7ba559805d0ee2d764e293a346fe5043cccf6323
Pushpak <pushpakagrawal123@gmail.com>
2023-11-12 18:32:06 +0530

    updated files

--- app.py
+++ app.py
@@ -4,23 +4,35 @@ import pickle
 import numpy as np
 from flask import Flask, Response
 
-app = Flask(__name__)
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-ip = "192.168.131.101"
-port = 2222
-s.bind((ip, port))
-
+s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ip = "192.168.1.5"
+port1 = 6666
+port2 = 6668
+s1.bind((ip, port1))
+s2.bind((ip, port2))
 
 while True:
-    x = s.recvfrom(1000000)
-    data = x[0]
-    data = pickle.loads(data)
-    frame = cv2.imdecode(data, cv2.IMREAD_COLOR)
-    cv2.imshow("IMG_SERVER",frame)
-    
+    x1 = s1.recvfrom(1000000)
+    x2 = s2.recvfrom(1000001)
+    data1 = x1[0]
+    data2 = x2[0]
+    data1 = pickle.loads(data1)
+    data2 = pickle.loads(data2)
+    frame1 = cv2.imdecode(data1, cv2.IMREAD_COLOR)
+    frame2 = cv2.imdecode(data2, cv2.IMREAD_COLOR)
+
+    # Resize frames to have the same height (assuming both frames have the same width)
+    min_height = min(frame1.shape[0], frame2.shape[0])
+    frame1 = cv2.resize(frame1, (int(frame1.shape[1] * min_height / frame1.shape[0]), min_height))
+    frame2 = cv2.resize(frame2, (int(frame2.shape[1] * min_height / frame2.shape[0]), min_height))
+
+    # Concatenate frames horizontally
+    concatenated_frame = np.concatenate((frame1, frame2), axis=1)
+
+    cv2.imshow("Concatenated Frames", concatenated_frame)
+
     if cv2.waitKey(5) & 0xFF == 27:
         break
+
 cv2.destroyAllWindows()
-
-
--- camera_client.py
+++ camera_client.py
@@ -7,22 +7,30 @@ import numpy as np
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET,socket.SO_SNDBUF,1000000)
 
-server_ip = "127.0.0.1"
-server_port = 5000
-
-cap = cv2.VideoCapture(0)
+server_ip = "192.168.1.5"
+server_port1 = 6666
+server_port2 = 6668
+cap = cv2.VideoCapture(1)
+cap1 = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
+cap1.set(3,640)
+cap1.set(4,480)
 
 while cap.isOpened():
     ret, img  = cap.read()
+    ret1, img1 = cap1.read()
     cv2.putText(img,"HELLO",(0,20),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,0),2)
     cv2.imshow("IMG_CLIENT", img)
+    cv2.imshow("IMG_CLIENT_1",img1)
 
     ret, buffer = cv2.imencode(".jpg",img, [int(cv2.IMWRITE_JPEG_QUALITY),30])
+    ret1, buffer1 = cv2.imencode(".jpg",img1, [int(cv2.IMWRITE_JPEG_QUALITY),30])
     x_as_bytes = pickle.dumps(buffer)
+    x_as_bytes1 = pickle.dumps(buffer1)
 
-    s.sendto((x_as_bytes),(server_ip,server_port))
+    s.sendto((x_as_bytes),(server_ip,server_port1))
+    s.sendto((x_as_bytes1),(server_ip,server_port2))
 
     if cv2.waitKey(5) & 0xFF == 27:
         break
